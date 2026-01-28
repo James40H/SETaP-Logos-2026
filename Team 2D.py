54cfe0d435186bf64c461e597858bd7a2d6d660a
James  Hind <jameshind2005@gmail.com>
2026-01-28 11:54:10 +0000

    Add outer gold ring to turtle drawing

--- Team 2D.py
+++ Team 2D.py
@@ -8,3 +8,13 @@ t = turtle.Turtle()
 t.hideturtle()
 t.speed(0)
 t.width(8)
+
+#Outer gold ring
+t.penup()
+t.goto(140, 70)     # RIGHT (+x) and UP (+y)
+t.setheading(110)    # Cut at 90 degrees (top)
+t.pendown()
+
+t.width(10)
+t.pencolor("#d4a23a")
+t.circle(150, 340)  # 270° arc → 90° gap
